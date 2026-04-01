cf39841f0af2a061bb37689b046ad8a13f81061b
Gavin Wald <gwald@umich.edu>
2026-03-31 23:36:01 -0400

    Implemented first two functions

--- startercode.py
+++ startercode.py
@@ -36,7 +36,11 @@ def load_json(filename):
         A dictionary with the JSON data, OR an empty dictionary {} if the file
         cannot be opened or is not valid JSON.
     """
-    pass
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
 
 
 def create_cache(dictionary, filename):
@@ -51,7 +55,8 @@ def create_cache(dictionary, filename):
     RETURNS:
         None
     """
-    pass
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(dictionary, f)
 
 
 def search_breed(breed_id):
